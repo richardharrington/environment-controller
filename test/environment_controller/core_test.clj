f1f749c08540e68ecbf94e8483c51ef709272664
Richard Harrington <harrington.richard@gmail.com>
2014-06-11 20:55:09 -0400

    first commit

---
+++ test/environment_controller/core_test.clj
@@ -0,0 +1,7 @@
+(ns environment-controller.core-test
+  (:require [clojure.test :refer :all]
+            [environment-controller.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
