4da9d6b630c629c9d1597c78e24d5d0fe018e33e
Richard Harrington <harrington.richard@gmail.com>
2014-06-11 22:32:40 -0400

    refactoring to get an hvac map

--- src/environment_controller/core.clj
+++ src/environment_controller/core.clj
@@ -1,7 +1,7 @@
 (ns environment-controller.core)
 
-(defn tic [get-temp-from-hvac]
-  (let [temp (get-temp-from-hvac)]
+(defn tic [{get-temp :get-temp :as hvac}]
+  (let [temp (get-temp)]
     (cond
      (< temp 65) {:cool nil, :heat true, :fan true}
      (> temp 75) {:cool true, :heat nil, :fan true}
--- test/environment_controller/core_test.clj
+++ test/environment_controller/core_test.clj
@@ -8,17 +8,17 @@
 
 (deftest test-tic-returns-correct-map-of-states-when-just-right
   (testing "tic returns map of off-states when hvac has 70 degree temp"
-    (is (= (tic (constantly 70))
+    (is (= (tic {:get-temp (constantly 70)})
         {:cool nil, :heat nil, :fan nil}))))
 
 (deftest test-tic-returns-correct-map-of-states-when-too-hot
   (testing "tic returns map of states (cool on, fan on, heat off) when hvac has 76 degree temp"
-    (is (= (tic (constantly 76))
+    (is (= (tic {:get-temp (constantly 76)})
            {:cool true, :heat nil, :fan true}))))
 
 (deftest test-tic-returns-correct-map-of-states-when-too-cold
   (testing "tic returns map of states (cool off, fan on, heat on) when hvac has 64 degree temp"
-    (is (= (tic (constantly 64))
+    (is (= (tic {:get-temp (constantly 64)})
            {:cool nil, :heat true, :fan true}))))
 
 (run-tests 'environment-controller.core-test)
