b77c220731e04edc1c7827650ff10fc802ed0df5
Richard Harrington <harrington.richard@gmail.com>
2014-06-14 08:51:55 -0400

    changed naming of count, countdown and countdown-store

--- src/environment_controller/core.clj
+++ src/environment_controller/core.clj
@@ -7,40 +7,42 @@
 (def upper-temp-limit (+ perfect-temp tolerance))
 
 
-(def heater-countdown (atom 0))
-(def cooler-countdown (atom 0))
+(def heater-countdown-store (atom 0))
+(def cooler-countdown-store (atom 0))
 
 
-(defn get-next-states [states temp cooler-count heater-count]
+(defn get-next-states [states temp cooler-countdown heater-countdown]
   (let [too-cold? (< temp lower-temp-limit)
         too-hot? (> temp upper-temp-limit)]
     {:heater too-cold?
-     :cooler (and too-hot? (= cooler-count 0))
-     :blower (or too-hot? too-cold? (> heater-count 0))}))
+     :cooler (and too-hot? (= cooler-countdown 0))
+     :blower (or too-hot? too-cold? (> heater-countdown 0))}))
 
-(defn get-next-heater-countdown [next-heater-state heater-count]
+(defn get-next-heater-countdown [next-heater-state heater-countdown]
   (cond
    next-heater-state 5
-   (> heater-count 0) (dec heater-count)
-   :else heater-count))
+   (> heater-countdown 0) (dec heater-countdown)
+   :else heater-countdown))
 
-(defn get-next-cooler-countdown [cooler-state next-cooler-state cooler-count]
+(defn get-next-cooler-countdown [cooler-state next-cooler-state cooler-countdown]
   (cond
    (and cooler-state (not next-cooler-state)) 2
-   (> cooler-count 0) (dec cooler-count)
-   :else cooler-count))
+   (> cooler-countdown 0) (dec cooler-countdown)
+   :else cooler-countdown))
 
 (defn tic! [hvac]
   (let [{:keys [states set-states! get-temp]} @hvac
         temp (get-temp)
         cooler-state (:cooler states)
+        heater-countdown @heater-countdown-store
+        cooler-countdown @cooler-countdown-store
         {next-heater-state :heater, next-cooler-state :cooler :as next-states}
-          (get-next-states states temp @cooler-countdown @heater-countdown)
-        next-heater-countdown (get-next-heater-countdown next-heater-state @heater-countdown)
-        next-cooler-countdown (get-next-cooler-countdown cooler-state next-cooler-state @cooler-countdown)]
+          (get-next-states states temp cooler-countdown heater-countdown)
+        next-heater-countdown (get-next-heater-countdown next-heater-state heater-countdown)
+        next-cooler-countdown (get-next-cooler-countdown cooler-state next-cooler-state cooler-countdown)]
 
-    (reset! heater-countdown next-heater-countdown)
-    (reset! cooler-countdown next-cooler-countdown)
+    (reset! heater-countdown-store next-heater-countdown)
+    (reset! cooler-countdown-store next-cooler-countdown)
     (set-states! next-states)))
 
 
