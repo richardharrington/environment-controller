9da54da26c1f7c71f9954ce926f7da2a374b87cd
Richard Harrington <harrington.richard@gmail.com>
2014-06-14 09:09:00 -0400

    changed '-state' to '-on?' for the device-state booleans

--- src/environment_controller/core.clj
+++ src/environment_controller/core.clj
@@ -11,35 +11,35 @@
 (def cooler-countdown-store (atom 0))
 
 
-(defn get-next-states [states temp cooler-countdown heater-countdown]
+(defn get-next-states [temp cooler-countdown heater-countdown]
   (let [too-cold? (< temp lower-temp-limit)
         too-hot? (> temp upper-temp-limit)]
-    {:heater too-cold?
-     :cooler (and too-hot? (= cooler-countdown 0))
-     :blower (or too-hot? too-cold? (> heater-countdown 0))}))
+    {:heater-on? too-cold?
+     :cooler-on? (and too-hot? (= cooler-countdown 0))
+     :blower-on? (or too-hot? too-cold? (> heater-countdown 0))}))
 
-(defn get-next-heater-countdown [next-heater-state heater-countdown]
+(defn get-next-heater-countdown [next-heater-on? heater-countdown]
   (cond
-   next-heater-state 5
+   next-heater-on? 5
    (> heater-countdown 0) (dec heater-countdown)
    :else heater-countdown))
 
-(defn get-next-cooler-countdown [cooler-state next-cooler-state cooler-countdown]
+(defn get-next-cooler-countdown [cooler-on? next-cooler-on? cooler-countdown]
   (cond
-   (and cooler-state (not next-cooler-state)) 2
+   (and cooler-on? (not next-cooler-on?)) 2
    (> cooler-countdown 0) (dec cooler-countdown)
    :else cooler-countdown))
 
 (defn tic! [hvac]
   (let [{:keys [states set-states! get-temp]} @hvac
-        temp (get-temp)
-        cooler-state (:cooler states)
+        cooler-on? (:cooler-on? states)
         heater-countdown @heater-countdown-store
         cooler-countdown @cooler-countdown-store
-        {next-heater-state :heater, next-cooler-state :cooler :as next-states}
-          (get-next-states states temp cooler-countdown heater-countdown)
-        next-heater-countdown (get-next-heater-countdown next-heater-state heater-countdown)
-        next-cooler-countdown (get-next-cooler-countdown cooler-state next-cooler-state cooler-countdown)]
+        temp (get-temp)
+        {next-heater-on? :heater-on?, next-cooler-on? :cooler-on? :as next-states}
+          (get-next-states temp cooler-countdown heater-countdown)
+        next-heater-countdown (get-next-heater-countdown next-heater-on? heater-countdown)
+        next-cooler-countdown (get-next-cooler-countdown cooler-on? next-cooler-on? cooler-countdown)]
 
     (reset! heater-countdown-store next-heater-countdown)
     (reset! cooler-countdown-store next-cooler-countdown)
--- test/environment_controller/core_test.clj
+++ test/environment_controller/core_test.clj
@@ -3,14 +3,10 @@
             [environment-controller.core :refer :all]
             [environment-controller.hvac]))
 
-; TODO: find out whether we're supposed to be querying
-; the hvac device for its device states, or whether we
-; keep track of that entirely ourselves -- i.e., do
-; we have getters, or just setters?
-
 (def hot (+ perfect-temp tolerance 1))
 (def cold (- perfect-temp tolerance 1))
 (def moderate perfect-temp)
+
 
 
 (defn fixtures [f]
@@ -25,9 +21,9 @@
     (swap! hvac merge {:set-states! (partial swap! hvac assoc :states)
                        :set-temp! (fn [temp]
                                     (swap! hvac assoc :get-temp (constantly temp)))
-                       :states {:heater false
-                                :cooler false
-                                :blower false}})
+                       :states {:heater-on? false
+                                :cooler-on? false
+                                :blower-on? false}})
     hvac))
 
 (defn execute-tics-with-temps! [hvac temp-sequence]
@@ -51,57 +47,57 @@
   (testing "tic does nothing to hvac states when :get-temp returns 70 degrees"
     (assert-temp-sequence-leads-to-states
      [moderate]
-     {:heater false
-      :cooler false
-      :blower false})))
+     {:heater-on? false
+      :cooler-on? false
+      :blower-on? false})))
 
 (deftest test-tic-turns-on-cooler-and-blower-when-temp-starts-out-too-high
   (testing "tic turns on cooler and blower when :get-temp returns 76 degrees"
     (assert-temp-sequence-leads-to-states
      [hot]
-     {:heater false
-      :cooler true
-      :blower true})))
+     {:heater-on? false
+      :cooler-on? true
+      :blower-on? true})))
 
 (deftest test-tic-turns-on-heater-and-blower-when-temp-starts-out-too-low
   (testing "tic turns on heater and blower when :get-temp returns 64 degrees"
     (assert-temp-sequence-leads-to-states
      [cold]
-     {:heater true
-      :cooler false
-      :blower true})))
+     {:heater-on? true
+      :cooler-on? false
+      :blower-on? true})))
 
 (deftest test-tic-keeps-blower-on-till-heater-cools-down
   (testing "blower stays on even under moderate conditions if heater has been off for less than 5 tics"
     (assert-temp-sequence-leads-to-states
      [cold moderate nil nil nil nil]
-     {:heater false
-      :cooler false
-      :blower true})))
+     {:heater-on? false
+      :cooler-on? false
+      :blower-on? true})))
 
 (deftest test-tic-turns-blower-off-after-heater-cools-down
   (testing "blower turns off under moderate conditions if heater has been off for at least 5 tics"
     (assert-temp-sequence-leads-to-states
      [cold moderate nil nil nil nil nil]
-     {:heater false
-      :cooler false
-      :blower false})))
+     {:heater-on? false
+      :cooler-on? false
+      :blower-on? false})))
 
 (deftest test-tic-keeps-cooler-off-until-its-ready-to-start-up-again
   (testing "cooler stays off even under hot conditions if it's been off for less than 3 tics"
     (assert-temp-sequence-leads-to-states
      [hot moderate hot nil]
-     {:heater false
-      :cooler false
-      :blower true})))
+     {:heater-on? false
+      :cooler-on? false
+      :blower-on? true})))
 
 (deftest test-tic-turns-cooler-on-if-its-too-hot-and-cooler-has-been-off-long-enough
   (testing "cooler turns on under hot conditions if it's been off for at least 3 tics"
     (assert-temp-sequence-leads-to-states
      [hot moderate hot nil nil]
-     {:heater false
-      :cooler true
-      :blower true})))
+     {:heater-on? false
+      :cooler-on? true
+      :blower-on? true})))
 
 
 (run-tests 'environment-controller.core-test)
